145a2296b777623608e6e38abbd97e058d5e44d8
Adesina Joshua <tolujosh1@gmail.com>
2024-03-05 16:18:06 +0100

    feature: corrections for default filters for project

--- project/settings.py
+++ project/settings.py
@@ -174,7 +174,8 @@ AUTHENTICATION_BACKENDS = (
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     
-    'DEFAULT_FILTER_BACKENDS': 'django_filters.rest_framework.DjangoFilterBackend',
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+
     
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
