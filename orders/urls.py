c94c95af69c5b8aaff0fc20e1816b2248eca5e9d
Adesina Joshua <tolujosh1@gmail.com>
2024-03-05 01:30:39 +0100

    feature: integrating paystack

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from orders import webhooks as wh
 from orders import views
 
 urlpatterns = [
     path('checkout/', views.checkout),
     path('orders/', views.OrdersList.as_view()),  
+    path('webhook/', wh.stack_webhook, name='stack-webhook'),
 ]
